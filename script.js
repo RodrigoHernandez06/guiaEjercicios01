c0c8946844d7d5c8550a80b02bb68af5e0381622
RodrigoHernandez06 <00018024@uca.edu.sv>
2025-09-24 08:27:52 -0600

    💄: Haciendo cambios al style

--- script.js
+++ script.js
@@ -1,55 +1,59 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const planes = [
-        {
-        nombre: "Básico",
-        precio: "$9.99 / mes",
-        slogan: "Ideal para empezar",
-        caracteristicas: [
-          "1 usuario",
-          "5 GB de almacenamiento",
-          "Soporte básico",
-          "Acceso limitado"
-        ]
-      },
-      {
-        nombre: "Intermedio",
-        precio: "$19.99 / mes",
-        slogan: "Perfecto para equipos pequeños",
-        caracteristicas: [
-          "Hasta 5 usuarios",
-          "50 GB de almacenamiento",
-          "Soporte prioritario",
-          "Acceso a funciones extra"
-        ]
-      },
-      {
-        nombre: "Avanzado",
-        precio: "$29.99 / mes",
-        slogan: "Para empresas y profesionales",
-        caracteristicas: [
-          "Usuarios ilimitados",
-          "500 GB de almacenamiento",
-          "Soporte premium 24/7",
-          "Todas las funciones incluidas"
-        ]
-      }
-    ];
+  const planes = [
+    {
+      nombre: "BASICO",
+      precio: "$9.99 / mes",
+      slogan: "Ideal para empezar",
+      caracteristicas: [
+        "1 usuario",
+        "5 GB de almacenamiento",
+        "Soporte básico",
+        "Acceso limitado"
+      ]
+    },
+    {
+      nombre: "INTERMEDIO",
+      precio: "$19.99 / mes",
+      slogan: "Perfecto para equipos pequeños",
+      caracteristicas: [
+        "Hasta 5 usuarios",
+        "50 GB de almacenamiento",
+        "Soporte prioritario",
+        "Acceso a funciones extra"
+      ]
+    },
+    {
+      nombre: "AVANZADO",
+      precio: "$29.99 / mes",
+      slogan: "Para empresas y profesionales",
+      caracteristicas: [
+        "Usuarios ilimitados",
+        "500 GB de almacenamiento",
+        "Soporte premium 24/7",
+        "Todas las funciones incluidas"
+      ]
+    }
+  ];
 
-    const contenedor = document.getElementById("caja");
+  const contenedor = document.getElementById("caja");
 
-    const renderizarPlanes = () => {
-      contenedor.innerHTML = planes.map(plan => `
+  const renderizarPlanes = () => {
+    contenedor.innerHTML = planes.map(plan => `
         <div class="card">
-          <h2>${plan.nombre}</h2>
-          <p class="price">${plan.precio}</p>
+        <div class="title">
+          <p class="name">${plan.nombre}</p>
+          <h1 class="price">${plan.precio}</h1>
           <p class="slogan">${plan.slogan}</p>
-          <ul>
+        </div>
+        <div class="body">
+          <ul class="list">
             ${plan.caracteristicas.map(c => `<li>${c}</li>`).join("")}
           </ul>
           <button id="boton">Seleccionar</button>
         </div>
+        </div>
       `).join("");
-    };
+  };
 
-    renderizarPlanes();
+  renderizarPlanes();
 });
